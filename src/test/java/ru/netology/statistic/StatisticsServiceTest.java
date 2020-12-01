0caf235450eba9e5b5eeab99a973b34c6a996a5e
Mariya Grishkova <galeyevamv@gmail.com>
2020-12-01 19:51:21 +0300

    StatisticsServiceTest updated

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,15 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-    long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxAmongBigger() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {4, 8, 16, 2};
+        long expected = 16;
+        assertEquals (expected, service.findMax(incomesInBillions));
+    }
 }
